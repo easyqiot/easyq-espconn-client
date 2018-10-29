a44173977d04da4601e3c7b70ffaf1a029cf97cb
pylover <vahid.mardani@gmail.com>
2018-10-29 23:48:06 +0330

    Removing mqtt from main

--- user/user_main.c
+++ user/user_main.c
@@ -1,7 +1,6 @@
 #include "ets_sys.h"
 #include "driver/uart.h"
 #include "osapi.h"
-#include "mqtt.h"
 #include "wifi.h"
 #include "config.h"
 #include "debug.h"
@@ -47,7 +46,6 @@
 #error "The flash map is not supported"
 #endif
 
-MQTT_Client mqttClient;
 typedef unsigned long u32_t;
 static ETSTimer sntp_timer;
 
@@ -60,7 +58,6 @@ void sntpfn()
         //os_printf("did not get a valid time from sntp server\n");
     } else {
             os_timer_disarm(&sntp_timer);
-            MQTT_Connect(&mqttClient);
     }
 }
 
@@ -73,52 +70,7 @@ void wifiConnectCb(uint8_t status)
         os_timer_setfn(&sntp_timer, (os_timer_func_t *)sntpfn, NULL);
         os_timer_arm(&sntp_timer, 1000, 1);//1s
     } else {
-          MQTT_Disconnect(&mqttClient);
     }
-}
-
-void mqttConnectedCb(uint32_t *args)
-{
-    MQTT_Client* client = (MQTT_Client*)args;
-    INFO("MQTT: Connected\r\n");
-    MQTT_Subscribe(client, "/mqtt/topic/0", 0);
-    MQTT_Subscribe(client, "/mqtt/topic/1", 1);
-    MQTT_Subscribe(client, "/mqtt/topic/2", 2);
-
-    MQTT_Publish(client, "/mqtt/topic/0", "hello0", 6, 0, 0);
-    MQTT_Publish(client, "/mqtt/topic/1", "hello1", 6, 1, 0);
-    MQTT_Publish(client, "/mqtt/topic/2", "hello2", 6, 2, 0);
-
-}
-
-void mqttDisconnectedCb(uint32_t *args)
-{
-    MQTT_Client* client = (MQTT_Client*)args;
-    INFO("MQTT: Disconnected\r\n");
-}
-
-void mqttPublishedCb(uint32_t *args)
-{
-    MQTT_Client* client = (MQTT_Client*)args;
-    INFO("MQTT: Published\r\n");
-}
-
-void mqttDataCb(uint32_t *args, const char* topic, uint32_t topic_len, const char *data, uint32_t data_len)
-{
-    char *topicBuf = (char*)os_zalloc(topic_len+1),
-            *dataBuf = (char*)os_zalloc(data_len+1);
-
-    MQTT_Client* client = (MQTT_Client*)args;
-
-    os_memcpy(topicBuf, topic, topic_len);
-    topicBuf[topic_len] = 0;
-
-    os_memcpy(dataBuf, data, data_len);
-    dataBuf[data_len] = 0;
-
-    INFO("Receive topic: %s, data: %s \r\n", topicBuf, dataBuf);
-    os_free(topicBuf);
-    os_free(dataBuf);
 }
 
 static const partition_item_t at_partition_table[] = {
@@ -144,18 +96,6 @@ void user_init(void)
     os_delay_us(60000);
 
     CFG_Load();
-
-    MQTT_InitConnection(&mqttClient, sysCfg.mqtt_host, sysCfg.mqtt_port, sysCfg.security);
-    //MQTT_InitConnection(&mqttClient, "192.168.11.122", 1880, 0);
-
-    MQTT_InitClient(&mqttClient, sysCfg.device_id, sysCfg.mqtt_user, sysCfg.mqtt_pass, sysCfg.mqtt_keepalive, 1);
-    //MQTT_InitClient(&mqttClient, "client_id", "user", "pass", 120, 1);
-
-    MQTT_InitLWT(&mqttClient, "/lwt", "offline", 0, 0);
-    MQTT_OnConnected(&mqttClient, mqttConnectedCb);
-    MQTT_OnDisconnected(&mqttClient, mqttDisconnectedCb);
-    MQTT_OnPublished(&mqttClient, mqttPublishedCb);
-    MQTT_OnData(&mqttClient, mqttDataCb);
 
     WIFI_Connect(sysCfg.sta_ssid, sysCfg.sta_pwd, wifiConnectCb);
 
